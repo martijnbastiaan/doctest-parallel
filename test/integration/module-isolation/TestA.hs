martijnbastiaan/doctest

Add module isolation
Before this patch all tests would run in a single GHCi session. This
meant that some commands -specifically those not reset by `:reload`-
would affect tests in subsequent modules. This patch starts a GHCi
session for each module, therefore mitigating the problem.

test/integration/module-isolation/TestA.hs
@@ -1,0 +1,12 @@
+module TestB (foo) where
+
+{- $setup
+>>> :set -XTypeApplications
+-}
+
+-- | Example usage:
+--
+-- >>> foo @Int
+-- 3
+foo :: Num a => a
+foo = 3

martijnbastiaan/doctest-parallel

test/integration/module-isolation/TestA.hs
@@ -1,0 +1,14 @@
+module TestA (foo) where
+
+import TestB ()
+
+{- $setup
+>>> :set -XTypeApplications
+-}
+
+-- | Example usage:
+--
+-- >>> foo @Int
+-- 3
+foo :: Num a => a
+foo = 3