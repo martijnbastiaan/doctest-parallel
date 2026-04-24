4b59b2cc6e2c45de27992cd555d8ba291b0e3979
Vekhir <134215107+Vekhir@users.noreply.github.com>
2026-04-24 17:25:38 +0200

    fix: CI (#103)
    We want to make sure that every job is necessary and being run.
    For output jobs, a catch-all "all" job is created which depends on
    them. The other jobs need to be transitive dependencies of them.
    
    This structure creates a dependency graph. We can get all transitive
    dependencies of "all" via the "nx.descendants" function.

--- .github/scripts/all_check.py
+++ .github/scripts/all_check.py
@@ -11,18 +11,29 @@ Makes sure:
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import networkx as nx
 import sys
 import yaml
 
 CI_PATH = ".github/workflows/ci.yml"
 ALL_TEST = "all"
 
+def get_needs(job_data):
+  needs = job_data.get("needs", ())
+  if not isinstance(needs, list):
+    needs = [needs]
+  return needs
+
 def main():
   ci_yml_fp = open(CI_PATH, "r")
   ci_yml_parsed = yaml.load(ci_yml_fp, Loader=yaml.FullLoader)
 
-  all_jobs = set(ci_yml_parsed['jobs'].keys()) - {ALL_TEST}
-  all_needs = set(ci_yml_parsed["jobs"][ALL_TEST]["needs"])
+  needs_graph = nx.DiGraph()
+  needs_graph.add_nodes_from((job, {"needs" : get_needs(data)}) for job, data in ci_yml_parsed['jobs'].items())
+  needs_graph.add_edges_from([(j, n) for j, d in needs_graph.nodes(data=True) for n in d["needs"]])
+
+  all_jobs = set(needs_graph.nodes) - {ALL_TEST}
+  all_needs = set(nx.descendants(needs_graph, ALL_TEST))
 
   if all_jobs - all_needs:
     sys.exit(f"Not all jobs mentioned in {ALL_TEST}.needs: {all_jobs - all_needs}")
